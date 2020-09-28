7bce02f692023e43b7cb65a120668532b2cabe5b
raajatg <raajatg@andrew.cmu.edu>
2020-09-28 15:51:35 -0400

    added todo

--- main.js
+++ main.js
@@ -3,5 +3,21 @@ function addNewList() {
 }
 
 function addListItem() {
-    console.log('hello world');
+    const groceryList = document.getElementById("grocery-list");
+    const itemInput = document.getElementById("new-list-item");
+    const newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    groceryList.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked){
+        item.parentNode.style.textDecoration = "line-through";
+    } else {
+        item.parentNode.style.textDecoration = "none";
+    }
 }
